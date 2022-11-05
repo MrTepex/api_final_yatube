9508465d76aac8a6717afed64cb2598ecb2367fc
Mikhail <bardabary@gmail.com>
2022-11-05 13:11:51 +0400

    Исправления после ревью 1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView,\
+    TokenRefreshView, TokenVerifyView
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 v1_router = DefaultRouter()
@@ -10,7 +11,12 @@ v1_router.register(r'v1/follow', FollowViewSet, basename='follow')
 v1_router.register(r'v1/posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 
-
+jwt_patterns = [
+    path('create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
 urlpatterns = [
     path('', include(v1_router.urls)),
+    path('v1/jwt/', include(jwt_patterns)),
 ]
